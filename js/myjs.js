12e974de10fc3e788ccdfa021950846f2c2b980d
jaquinterocl <jaquinterocl@ufpso.edu.co>
2023-05-27 12:00:50 -0500

    add reponsive

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,19 @@
 const boton = document.querySelector('.botonnav ');
 const botonicono = document.querySelector('.botonnav i ');
 const menu = document.querySelector('.barra');
+const buscar = document.querySelector(".botonbuscar");
+const buscaricono = document.querySelector(".botonbuscar i");
+const buscador = document.querySelector(".buscar");
 
 boton.onclick = function (){
     menu.classList.toggle('open');
     const openyes = menu.classList.contains("open");
 
     botonicono.classList = openyes;
+}
+buscar.onclick = function (){
+    buscador.classList.toggle("open");
+    const openbuscar = buscador.classList.contains("open");
+
+    buscaricono.classList = openbuscar;
 }
